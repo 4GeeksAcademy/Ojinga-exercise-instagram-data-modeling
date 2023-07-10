e7c872cf2325a02ccce3eab80dbfcfcb87dccbc0
GISOjinga <103523730+GISOjinga@users.noreply.github.com>
2023-07-10 00:30:17 +0000

    Done

--- src/models.py
+++ src/models.py
@@ -7,31 +7,53 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+# User model with fields for id, username, first name, last name, and email.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    first_name = Column(String(250))
+    last_name = Column(String(250))
+    email = Column(String(250), nullable=False)
+    posts = relationship('Post', backref='user')
+    comments = relationship('Comment', backref='user')
+    followers = relationship('Follower', backref='user')
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+# Post model with fields for id and user_id.
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    media = relationship('Media', backref='post')
+    comments = relationship('Comment', backref='post')
 
-    def to_dict(self):
-        return {}
+# Media model with fields for id, type, url, and post_id.
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(250))
+    url = Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+# Comment model with fields for id, comment_text, author_id, and post_id.
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250))
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+# Follower model with fields for user_from_id and user_to_id.
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer)
 
 ## Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
-    print("There was a problem genering the diagram")
+    print("There was a problem generating the diagram")
     raise e
